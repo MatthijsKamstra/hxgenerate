9489720ace422f5f7116bf599e1611bf4902750e
Matthijs Kamstra aka [mck] <matthijs@fonkmobile.nl>
2016-03-27 01:23:15 +0100

    added example folder to bin

---
+++ bin/example_project/src/Main.hx
@@ -0,0 +1,18 @@
+package;
+
+
+/**
+ * @author Matthijs Kamstra aka[mck]
+ * MIT
+ * http://www.matthijskamstra.nl/
+ */ 
+class Main {
+	
+	public function new () {
+		trace( "Hello Example project" );
+	}
+
+	static public function main () {
+		var app = new Main ();
+	}
+}
