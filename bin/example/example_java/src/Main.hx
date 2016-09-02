a6088f2671a0b54a31a9cf7c3770422748bb8c2f
Matthijs Kamstra <matthijs@fonk.amsterdam>
2016-09-03 01:15:11 +0200

    ignore java export folder

--- bin/example/example_java/src/Main.hx
+++ bin/example/example_java/src/Main.hx
@@ -3,12 +3,14 @@ package;
 /**
  * @author Matthijs Kamstra aka[mck]
  * MIT
- * 
- */ 
+ *
+ */
 class Main {
-	
+
 	public function new () {
 		trace( "Hello \'Example Java\'" );
+		java.Lib.println('println');
+		java.Lib.print('print');
 	}
 
 	static public function main () {
